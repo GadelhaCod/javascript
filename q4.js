f0c158e9e416ef9e2373d13ef568450a4e6518c0
Gadelha <103457436+GadelhaCod@users.noreply.github.com>
2022-04-12 10:19:05 -0300

    Delete q4.js

--- q4.js
+++
@@ -1,4 +0,0 @@
-function clic(){
-    var num = parseInt(prompt("Digite o numúmero aqui: "));
-   document.querySelector("p").innerHTML="O antecessor de " + num + " é " + (num -1) + " e o seu sucessor é " + (num +1);
-}
